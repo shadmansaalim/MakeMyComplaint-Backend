abdd29b50920cf1dcbef677fddfb89042dd7fbe6
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-09 14:18:26 +1000

    API setup to add users to database

--- index.js
+++ index.js
@@ -24,6 +24,27 @@ async function run() {
         await client.connect();
         const database = client.db("makemycomplaint");
         const storesCollection = database.collection("stores");
+        const usersCollection = database.collection("users");
+
+        //Add users to database those who signed up with Email Password
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        })
+
+        //Add users to database those who signed up with External Provider
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+
+        })
 
 
 
